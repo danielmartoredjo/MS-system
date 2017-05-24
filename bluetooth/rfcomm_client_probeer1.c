13526bf22aad603d85f1c5aef0e5460631547fdf
Tekakato <daan.conijn@live.nl>
2017-05-24 17:03:16 +0200

    meerdere msgs V0.1 werkt nog niet

--- bluetooth/rfcomm_client_probeer1.c
+++ bluetooth/rfcomm_client_probeer1.c
@@ -9,7 +9,7 @@ int main(int argc, char **argv)
     struct sockaddr_rc addr = { 0 };
     int s, status;
     char dest[18] = "B8:27:EB:AF:FF:31";
-
+//while(1){
     // allocate a socket
     s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
@@ -17,17 +17,22 @@ int main(int argc, char **argv)
     addr.rc_family = AF_BLUETOOTH;
     addr.rc_channel = (uint8_t) 1;
     str2ba( dest, &addr.rc_bdaddr );
-
+//while(1){
     // connect to server
     status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
-
+while(1){
     // send a message
     if( status == 0 ) {
-        status = write(s, "hello!", 6);
-    }
+        status = write(s, "exit", 6);
+	printf("send\n");    
+	}
 
-    if( status < 0 ) perror("uh oh");
+    if( status < 0 ){
+	 perror("uh oh");
+	break;
+	}
+	sleep(5);
+}    close(s);
 
-    close(s);
     return 0;
 }
