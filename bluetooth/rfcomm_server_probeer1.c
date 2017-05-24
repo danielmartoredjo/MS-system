db20b2ca7da74b5c7590ef2c114123fcd6df39c3
AutoTrein1617 <AutoTrein1617@github.com>
2017-05-24 16:51:17 +0200

    Proberen meerdere dingen te ontvangen en te sluiten bij exit (beide werkt niet)

--- bluetooth/rfcomm_server_probeer1.c
+++ bluetooth/rfcomm_server_probeer1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <bluetooth/bluetooth.h>
@@ -7,10 +8,10 @@
 int main(int argc, char **argv)
 {
     struct sockaddr_rc loc_addr = { 0 }, rem_addr = { 0 };
-    char buf[1024] = { 0 };
-    int s, client, bytes_read;
+    char buf[1024] = { 0 }, message[20] = {0};
+    int s, client, bytes_read, i;
     socklen_t opt = sizeof(rem_addr);
-
+//while(1){
     // allocate socket
     s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
@@ -21,23 +22,35 @@ int main(int argc, char **argv)
     loc_addr.rc_channel = (uint8_t) 1;
     bind(s, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
 
-    // put socket into listening mode
-    listen(s, 1);
+    while(1){
+        // put socket into listening mode
+        listen(s, 1);
+//     while(1){
+        // accept one connection
+        client = accept(s, (struct sockaddr *)&rem_addr, &opt);
 
-    // accept one connection
-    client = accept(s, (struct sockaddr *)&rem_addr, &opt);
+        ba2str( &rem_addr.rc_bdaddr, buf );
+//        fprintf(stderr, "accepted connection from %s\n", buf);
+        memset(buf, 0, sizeof(buf));
 
-    ba2str( &rem_addr.rc_bdaddr, buf );
-    fprintf(stderr, "accepted connection from %s\n", buf);
-    memset(buf, 0, sizeof(buf));
+    	// read data from the client
+    	bytes_read = read(client, buf, sizeof(buf));
+	i = 0;
+    	if( bytes_read > 0 ) {
+//		printf("iets");
+        	printf("received [%s]\n", buf);
+		message[i] = buf;
+    	}
+	if(strncmp(message,"exit", 4) == 0) {
+		break;
+	}
 
-    // read data from the client
-    bytes_read = read(client, buf, sizeof(buf));
-    if( bytes_read > 0 ) {
-        printf("received [%s]\n", buf);
-    }
 
     // close connection
+        close(client);
+//    }
+        close(s);
+    }
     close(client);
     close(s);
     return 0;
