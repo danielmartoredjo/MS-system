11b7e9c88075541d4d27984704224d16c0074ffd
Tekakato <daan.conijn@live.nl>
2017-05-29 13:54:23 +0200

    Fixed layout and added macro for the Server address

--- bluetooth/rfcomm_client.c
+++ bluetooth/rfcomm_client.c
@@ -1,15 +1,20 @@
-//versie 1.0 
+/*
+ * File: rfcomm_client.c
+ * version: 1.1 
+ */
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/rfcomm.h>
 
+#define SERVER_ADDR "B8:27:EB:5D:7B:C9"
+
 int main(int argc, char **argv)
 {
     struct sockaddr_rc addr = { 0 };
     int s, status;
-    char dest[18] = "B8:27:EB:5D:7B:C9";
+    char dest[18] = SERVER_ADDR;
 
     // allocate a socket
     s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
@@ -21,19 +26,21 @@ int main(int argc, char **argv)
 
     // connect to server
     status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
-int i;
-char message[]="hello:000";
+    int i;
+    char message[]="hello:000";
     // send a message
-    if( status == 0 ) {
-while(1){
-	message[6]=(i/100)%10 + '0';
-	message[7]=(i/10)%10 + '0';
-	message[8]=i%10 + '0';
+    if( status == 0 ) 
+    {
+        while(1)
+        {
+        message[6]=(i/100)%10 + '0';
+        message[7]=(i/10)%10 + '0';
+        message[8]=i%10 + '0';
         status = write(s, message, 10);
-	//status = write(s, "hello2", 8);
-	i++;
+        //status = write(s, "hello2", 8);
+        i++;
+        }
     }
-}
 
     if( status < 0 ) perror("uh oh");
 
