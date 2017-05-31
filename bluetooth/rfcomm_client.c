34871d216d65255abf5d226ec9e0729df47b8d57
AutoTrein1617 <AutoTrein1617@github.com>
2017-05-31 12:03:53 +0200

    v 1.2 cleaned some code, bettered the while and if/else code

--- bluetooth/rfcomm_client.c
+++ bluetooth/rfcomm_client.c
@@ -12,57 +12,67 @@
 
 int connect_bluetooth(int* s, int* status);
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
 	int s, status;
-/*    struct sockaddr_rc addr = { 0 };
-    int s, status;
-    char dest[18] = SERVER_ADDR;
+        char message[]="hello:000";
+        int i = 0;
 
-    // allocate a socket
-    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+//	while(1){
+//		int s, status;
+//		char message[]="hello:000";
+//		int i;
 
-    // set the connection parameters (who to connect to)
-    addr.rc_family = AF_BLUETOOTH;
-    addr.rc_channel = (uint8_t) 1;
-    str2ba( dest, &addr.rc_bdaddr );
-
-    // connect to server
-    status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
-*/ 
 	connect_bluetooth(&s, &status);
-   	int i;
-    	char message[]="hello:000";
-    // send a message
-    	if( status == 0 ) 
-    	{
-        	while(1)
-        	{
-        	message[6]=(i/100)%10 + '0';
-        	message[7]=(i/10)%10 + '0';
-        	message[8]=i%10 + '0';
-		i++;
-        	status = write(s, message, 10);
- 		while(status < 0){
-			printf("retry connection %d\n", status);
-			sleep(2);
-			close(s);
-			connect_bluetooth(&s, &status);
-		}
- //status = write(s, "hello2", 8);
-        	}
-    	}
+	/*while(1){
+                // send a message
+                if( status == 0 ){
+                        message[6]=(i/100)%10 + '0';
+                        message[7]=(i/10)%10 + '0';
+                        message[8]=i%10 + '0';
+                        i++;
+                        status = write(s, message, 10);
+                        //status = write(s, "hello2", 8);
+                }
+                //Retry connecting
+//              while(status < 0){
+//                      printf("retry connection %d\n", status);
+//                      sleep(2);
+//                      close(s);
+//                      connect_bluetooth(&s, &status);
+//              }
 
-    	if( status < 0 ) perror("uh oh");{
-		close(s);
-		connect_bluetooth(&s, &status);
+                if( status < 0 ) perror("uh oh");{
+                        printf("retry connection %d\n", status);
+                        sleep(2);
+                        close(s);
+                        connect_bluetooth(&s, &status);
+                }
+        }*/
+
+	while(1){
+		if( status >= 0 ){
+			message[6]=(i/100)%10 + '0';
+                        message[7]=(i/10)%10 + '0';
+                        message[8]=i%10 + '0';
+                        i++;
+                        status = write(s, message, 10);
+                        //status = write(s, "hello2", 8);
+                }
+
+		// send a message
+    		else{
+			perror("uh oh");
+                        printf("retry connection %d\n", status);
+                        sleep(2);
+                        close(s);
+                        connect_bluetooth(&s, &status);
+		}
 	}
-    	//close(s);
     	return 0;
 }
 
 int connect_bluetooth(int *s, int *status){
-        struct sockaddr_rc addr = { 0 };
+	struct sockaddr_rc addr = { 0 };
         char dest[18] = SERVER_ADDR;
 
         // allocate a socket
